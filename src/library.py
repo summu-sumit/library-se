1866ccedb0a564b89d77f9ddea648c6750ed8b06
Sumit Sagar <sumitsagar1919@gmail.com>
2026-01-21 03:55:19 +0530

    Sprint 2: Borrow and return book functionality

--- src/library.py
+++ src/library.py
@@ -6,10 +6,21 @@ class Library:
     def __init__(self):
         self.books = {}
 
+    # Sprint-1
     def add_book(self, book_id, title, author):
         if book_id in self.books:
             raise LibraryError("Duplicate Book ID")
         self.books[book_id] = {
             "title": title,
-            "author": author
+            "author": author,
+            "borrowed": False
         }
+
+    # Sprint-2
+    def borrow_book(self, book_id):
+        if self.books[book_id]["borrowed"]:
+            raise LibraryError("Book already borrowed")
+        self.books[book_id]["borrowed"] = True
+
+    def return_book(self, book_id):
+        self.books[book_id]["borrowed"] = False
--- tests/test_library.py
+++ tests/test_library.py
@@ -2,8 +2,9 @@ import unittest
 from src.library import Library, LibraryError
 
 
-class TestLibrarySprint1(unittest.TestCase):
+class TestLibrary(unittest.TestCase):
 
+    # Sprint-1 Tests
     def test_add_book_success(self):
         lib = Library()
         lib.add_book("B1", "Clean Code", "Robert Martin")
@@ -14,6 +15,27 @@ class TestLibrarySprint1(unittest.TestCase):
         lib.add_book("B1", "Clean Code", "Robert Martin")
         with self.assertRaises(LibraryError):
             lib.add_book("B1", "Another Book", "Someone")
+
+    # Sprint-2 Tests
+    def test_borrow_available_book(self):
+        lib = Library()
+        lib.add_book("B1", "Clean Code", "Robert Martin")
+        lib.borrow_book("B1")
+        self.assertTrue(lib.books["B1"]["borrowed"])
+
+    def test_borrow_unavailable_book(self):
+        lib = Library()
+        lib.add_book("B1", "Clean Code", "Robert Martin")
+        lib.borrow_book("B1")
+        with self.assertRaises(LibraryError):
+            lib.borrow_book("B1")
+
+    def test_return_book(self):
+        lib = Library()
+        lib.add_book("B1", "Clean Code", "Robert Martin")
+        lib.borrow_book("B1")
+        lib.return_book("B1")
+        self.assertFalse(lib.books["B1"]["borrowed"])
 
 
 if __name__ == "__main__":
